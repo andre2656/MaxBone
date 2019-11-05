d3560ce5351f3a54bf76412f3a47db02aae30c3a
Andre Campbell <andre2656@gmail.com>
2019-11-04 19:22:42 -0700

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,8 @@ import './App.css'
 import MarketingSite from "./components/MarketingSite/MarketingSite"
 import SignUp from './components/SignUp/SignUp';
 import StyleApp from './components/StyleApp/StyleApp';
-import SettingsForm from './components/StyleApp/SettingsForm/SettingsForm';
-import Payment from './components/SignUp/Payment';
-import IconQuiz from './components/Quizzes/IconQuiz/IconQuiz';
-import Chat from './components/StyleApp/Chat/Index.js'
-import stylistChat from './components/StylistPage/MessageNav'
-import FashionStyleQuiz from './components/Quizzes/FashionStyleQuiz/FashionStyleQuiz';
 import loginController from "./controllers/LoginController";
-import SkinToneQuiz from "./components/Quizzes/SkinToneQuiz/SkinToneQuiz";
+
 // import Styles from './components/StyleApp/StyleRecognition/StyleRecognition'
 
 
@@ -44,14 +38,14 @@ class App extends Component {
         <div>
           <Route exact path="/" component={MarketingSite} />
           <Route exact path="/sign-up" component={SignUp} />
-          <Route exact path="/payment" component={Payment} />
-          <Route exact path="/settings" component={SettingsForm} />
+          {/* <Route exact path="/payment" component={Payment} /> */}
+          {/* <Route exact path="/settings" component={SettingsForm} /> */}
           <Route exact path="/app" component={StyleApp} />
-          <Route exact path="/iconq" component={IconQuiz} />
+          {/* <Route exact path="/iconq" component={IconQuiz} />
           <Route exact path="/fsq" component={FashionStyleQuiz} />
           <Route exact path= "/chat" component={Chat}/>
           <Route exact path="/stylistChat" component={stylistChat} />
-          <Route exact path="/stq" component={SkinToneQuiz} />
+          <Route exact path="/stq" component={SkinToneQuiz} /> */}
           {/* <Route exact path= '/styles' component={Styles}/> */}
 
         </div>
